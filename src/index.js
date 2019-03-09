679aa2530dbf0ba1b64c01b49ade5ca56c0473db
Kirill Anisimov <akpoflash@gmail.com>
2019-03-09 10:56:19 +0300

    Add crud methods with mongodb

--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
+let db;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// mock for db
-let books = [
-	{
-		id: 1,
-		name: 'Война и мир',
-	},
-	{
-		id: 2,
-		name: 'Тихий Дон',
-	},
-	{
-		id: 3,
-		name: 'Сумерки',
-	},
-];
 
 app.get('/', (req, res) => {
 	res.send('API');
@@ -28,38 +15,76 @@ app.get('/', (req, res) => {
 
 // get all books
 app.get('/books', (req, res) => {
-	res.send(books);
+	db.collection('books')
+		.find()
+		.toArray((err, docs) => {
+			if (err) {
+				console.error(err);
+				return res.sendStatus(500);
+			}
+			res.send(docs);
+		});
 });
 
 // get book by id
 app.get('/books/:id', (req, res) => {
-	const book = books.find(book => book.id === +req.params.id);
-	res.send(book);
+	db.collection('books').findOne({ _id: ObjectId(req.params.id) }, (err, book) => {
+		if (err) {
+			console.error(err);
+			return res.sendStatus(500);
+		}
+		res.send(book);
+	});
 });
 
 // add new book
 app.post('/books', (req, res) => {
 	const book = {
-		id: Date.now(),
 		name: req.body.name,
 	};
-	books.push(book);
+
+	db.collection('books').insertOne(book, (err, result) => {
+		if (err) {
+			console.error(err);
+			return res.sendStatus(500);
+		}
+	});
+
 	res.sendStatus(200);
 });
 
 // update book
-app.post('/books/:id', (req, res) => {
-	const book = books.find(book => book.id === +req.params.id);
-	book.name = req.body.name;
-	res.sendStatus(200);
+app.put('/books/:id', (req, res) => {
+	db.collection('books').updateOne(
+		{ _id: ObjectId(req.params.id) },
+		{ $set: { name: req.body.name } },
+		(err, book) => {
+			if (err) {
+				console.error(err);
+				return res.sendStatus(500);
+			}
+			res.sendStatus(200);
+		}
+	);
 });
 
 // delete book
 app.delete('/books/:id', (req, res) => {
-	books = books.filter(book => book.id !== +req.params.id);
-	res.sendStatus(200);
+	db.collection('books').deleteOne({ _id: ObjectId(req.params.id) }, (err, book) => {
+		if (err) {
+			console.error(err);
+			return res.sendStatus(500);
+		}
+		res.sendStatus(200);
+	});
 });
 
-app.listen(3012, () => {
-	console.log('API start');
+MongoClient.connect('mongodb://localhost:27017/api', { useNewUrlParser: true }, (err, database) => {
+	if (err) {
+		return console.error(err);
+	}
+	db = database.db('api');
+	app.listen(3012, () => {
+		console.log('API start');
+	});
 });
